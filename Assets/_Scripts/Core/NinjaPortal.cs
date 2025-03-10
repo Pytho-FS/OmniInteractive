cd44af5a097a3fff2e058ffdc9917e4b1f63bbd8
Brandon Grasso <grassobrandon323@gmail.com>
2025-03-10 07:40:14 -0400

    merge stuff

--- Assets/_Scripts/Core/NinjaPortal.cs
+++ Assets/_Scripts/Core/NinjaPortal.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 public class NinjaPortal : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NinjaPortal : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] ParticleSystem particleSys;
 
     [Header("Text Fields")]
@@ -21,8 +29,34 @@ public class NinjaPortal : MonoBehaviour
     }
     private void Start()
     {
+    [Header("Text Fields")]
+    [SerializeField] private TMP_Text initialMsg;
+    [SerializeField] private TMP_Text finalMsg;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && GameManager.Instance.totalNinjas == 0)
+        {
+            GameManager.Instance.SetCurrentMiniGame(1);
+            GameManager.Instance.LoadScene("Main Scene");
+        }
+    }
+    private void Start()
+    {
         spriteRenderer.enabled = false;
         finalMsg.enabled = false;
+        initialMsg.enabled = true;
+
+    }
+    private void Update()
+    {
+        if (GameManager.Instance.totalNinjas==0)
+        {
+            spriteRenderer.enabled = true;
+            initialMsg.enabled = false;
+            finalMsg.enabled = true;
+        }
+    }
+}
         initialMsg.enabled = true;
 
     }
