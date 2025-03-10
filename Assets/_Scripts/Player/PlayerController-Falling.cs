0cce6d50d89f016cb9e1ff030906e06486e0906b
Max <166408857+LeGermen@users.noreply.github.com>
2025-03-09 20:31:20 -0400

    Falling Minigame - Player Controller
    Created controller specific to the player on Falling Minigame with basic horizontal movement

---
+++ Assets/_Scripts/Player/PlayerController-Falling.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerControllerFalling : MonoBehaviour {
+
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] float speed;
+    [SerializeField] float gravity;
+
+    Vector2 lastMoveDirection = Vector2.right;
+    Vector2 moveDir;
+    Vector2 playerVelocity;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+    private void Update()
+    {
+        movement();
+    }
+
+    void movement() 
+    { 
+        float moveX = Input.GetAxis("Horizontal");
+
+        // Set movement vector
+        moveDir = new Vector2(moveX, 0).normalized;
+        if (moveDir != Vector2.zero)
+        {
+            lastMoveDirection = moveDir;
+            rb.linearVelocity = new Vector2(moveDir.x * speed, rb.linearVelocity.y);
+        }
+
+        // Flip sprite on Y-axis based on movement direction
+        if (moveX != 0)
+        {
+            spriteRenderer.flipX = moveX < 0;
+        }
+    }
+}
+
