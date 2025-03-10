520ceb748755dba7cb0976cf37e90a9cb4f4ef4b
David Emmert <emmertd30@gmail.com>
2025-03-10 03:11:52 -0400

    ninja script

--- Assets/_Scripts/Core/NinjaNPC.cs
+++ Assets/_Scripts/Core/NinjaNPC.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 public class NinjaNPC : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
-    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] BoxCollider2D colliderNinja;
+    [SerializeField] private float moveX;
+    [SerializeField] private float moveY;
+    [SerializeField] private float moveZ;
     [SerializeField] private float moveSpeed = 2f; 
     [SerializeField] private float waitTime = 1f;
     private Vector3 startPosition;
+    private Vector3 targetPosition;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameManager.totalNinjas++;
+        startPosition = transform.position;
+        if (gameManager != null)
+        {
+            gameManager.totalNinjas++;
+        }
+        targetPosition.x = startPosition.x + moveX; 
+        targetPosition.y = startPosition.y + moveY;
+        targetPosition.z = startPosition.z + moveZ;
+        StartCoroutine(toggleNinja());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (colliderNinja.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            //talk to game, activate gate for opening function.
+        }
     }
     public void WasFound()
     {
@@ -28,7 +44,7 @@ public class NinjaNPC : MonoBehaviour
     {
         if (collision != null)
         {
-            if (collision.IsTouchingLayers(LayerMask.GetMask("Player")))
+            if (collision.IsTouchingLayers(LayerMask.GetMask("Ninja")))
             {
 
                 WasFound();
@@ -38,28 +54,35 @@ public class NinjaNPC : MonoBehaviour
     
     IEnumerator MoveNinja()
     {
-        // Move to target position
-        yield return StartCoroutine(LerpPosition(targetPosition, moveSpeed));
-
-        // Wait at the position
+        while (true)
+        {
+           // yield return StartCoroutine(LerpPosition(startPosition,targetPosition, moveSpeed));
+           transform.position = Vector3.Lerp(startPosition, targetPosition, 0.3f);
+            yield return new WaitForSeconds(waitTime);
+            //yield return StartCoroutine(LerpPosition(targetPosition,startPosition, moveSpeed));
+            transform.position = Vector3.Lerp(targetPosition, startPosition, 0.3f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+    IEnumerator toggleNinja()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        yield return new WaitForSeconds(waitTime);
+        gameObject.SetActive(true);
         yield return new WaitForSeconds(waitTime);
 
-        // Move back to the start position
-        yield return StartCoroutine(LerpPosition(startPosition, moveSpeed));
     }
-
-    IEnumerator LerpPosition(Vector3 target, float duration)
+    IEnumerator LerpPosition(Vector3 start, Vector3 target,float duration)
     {
         float time = 0;
-        Vector3 start = transform.position;
-
+        
         while (time < duration)
         {
-            transform.position = Vector3.Lerp(start, target, time / duration);
+            transform.position = Vector3.Lerp(start, target, time/duration);
             time += Time.deltaTime;
             yield return null;
         }
 
-        transform.position = target; // Ensure it snaps exactly to target
+        transform.position = start; // Ensure it snaps exactly to target
     }
 }
